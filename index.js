945291f581a24f3350843f2db0d2ddae99b56e65
prclima <pedro@futuranet.com.br>
2022-11-01 09:46:54 -0300

    botao restart funcionando

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const dinoDoido = document.querySelector('.dino');
-const cacto = document.querySelector('.cacto');
+let dinoDoido = document.querySelector('.dino');
+let cacto = document.querySelector('.cacto');
 const fogo = document.querySelector('.fogo');
 const btnPlay = document.getElementById('btnPlay');
 let imgBackground = document.querySelector('.background');
 const h2 = document.querySelector('h2')
 const btnRestart = document.getElementById('btnRestart')
+const gameOverDino = document.getElementById('gameOverDino')
 
 
 btnRestart.setAttribute("id", '')
+
 // Jogo começa com usuário apertando o botao Play.
 btnPlay.addEventListener('click', pressPlay)
 function pressPlay() { 
@@ -52,12 +54,14 @@ function cactoMoveIni() {
 const log  = setInterval(() => {
   let coordCacto = parseInt(window.getComputedStyle(cacto).getPropertyValue("left"))
  let coordTopDino = parseInt((window.getComputedStyle(dinoDoido).bottom)) 
- let statusGame = 0;
+ //let statusGame = 0;
     
    if (coordCacto > 855 && coordTopDino < 69) {
     cacto.style.animation = 'none'; 
     imgBackground = imgBackground.setAttribute("src", './imagem/backgroundBusted.jpg');
-    statusGame = 1;
+    btnRestart.setAttribute("id", 'btnRestart');
+    dinoDoido = dinoDoido.setAttribute("class", " ");
+    cacto = cacto.setAttribute("class", '');
 
    };
 
@@ -65,13 +69,12 @@ const log  = setInterval(() => {
 
  
 
-   const btnRefresh  = setInterval(() => { 
-   if (statusGame = 1) {
-    btnRestart.setAttribute("id", 'btnRestart');
-    statusGame = 0;
-    }
-   }, 7000)
-
+ 
    btnRestart.addEventListener('click', () =>  {
-    window.location.reload()
+    window.location.reload();
+    dinoDoido.setAttribute("class", 'dino');
+    cacto = cacto.setAttribute("class", 'cacto');
+
    })
+
+   //("src", '"./imagem/dino.gif');
