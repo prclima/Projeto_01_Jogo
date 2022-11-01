255b30cc93ba317e3a33274a784c3611ebc3d19d
prclima <pedro@futuranet.com.br>
2022-10-31 23:53:18 -0300

    botao restart

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const dinoDoido = document.querySelector('.dino');
 const cacto = document.querySelector('.cacto');
 const fogo = document.querySelector('.fogo');
-const btnPlay = document.querySelector('button');
-const imgBackground = document.querySelector('.background');
+const btnPlay = document.getElementById('btnPlay');
+let imgBackground = document.querySelector('.background');
 const h2 = document.querySelector('h2')
+const btnRestart = document.getElementById('btnRestart')
 
+
+btnRestart.setAttribute("id", '')
 // Jogo começa com usuário apertando o botao Play.
 btnPlay.addEventListener('click', pressPlay)
 function pressPlay() { 
@@ -49,10 +52,26 @@ function cactoMoveIni() {
 const log  = setInterval(() => {
   let coordCacto = parseInt(window.getComputedStyle(cacto).getPropertyValue("left"))
  let coordTopDino = parseInt((window.getComputedStyle(dinoDoido).bottom)) 
+ let statusGame = 0;
     
    if (coordCacto > 855 && coordTopDino < 69) {
-    cacto.style.animation = 'none';
-    imgBackground = imgBackground.setAttribute("src", "./imagem/backgroundBusted.jpg")
-   }
+    cacto.style.animation = 'none'; 
+    imgBackground = imgBackground.setAttribute("src", './imagem/backgroundBusted.jpg');
+    statusGame = 1;
+
+   };
 
   }, 10);
+
+ 
+
+   const btnRefresh  = setInterval(() => { 
+   if (statusGame = 1) {
+    btnRestart.setAttribute("id", 'btnRestart');
+    statusGame = 0;
+    }
+   }, 7000)
+
+   btnRestart.addEventListener('click', () =>  {
+    window.location.reload()
+   })
